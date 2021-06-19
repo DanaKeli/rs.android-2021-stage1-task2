391c9388d95838885eb62ca9a1e62b8d5003c5b1
DanaKeli <79215094+DanaKeli@users.noreply.github.com>
2021-06-19 19:45:56 +0300

    feat: complit task DateFormatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,39 @@
 package subtask1
 
-class DateFormatter {
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
 
-    // TODO: Complete the following function
+class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        try {
+            var date = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
+            var m = when (date.month) {
+                Month.JANUARY -> "января"
+                Month.FEBRUARY -> "февраля"
+                Month.MARCH -> "марта"
+                Month.APRIL -> "апреля"
+                Month.MAY -> "мая"
+                Month.JUNE -> "июня"
+                Month.JULY -> "июля"
+                Month.AUGUST -> "августа"
+                Month.SEPTEMBER -> "сентября"
+                Month.OCTOBER -> "октября"
+                Month.NOVEMBER -> "ноября"
+                Month.DECEMBER -> "декабря"
+            }
+            var dow = when (date.dayOfWeek) {
+                DayOfWeek.MONDAY -> "понедельник"
+                DayOfWeek.TUESDAY -> "вторник"
+                DayOfWeek.WEDNESDAY -> "среда"
+                DayOfWeek.THURSDAY -> "четверг"
+                DayOfWeek.FRIDAY -> "пятница"
+                DayOfWeek.SATURDAY -> "суббота"
+                DayOfWeek.SUNDAY -> "воскресенье"
+            }
+            return "$day $m, $dow"
+        } catch (e: Exception) {
+            return "Такого дня не существует"
+        }
     }
 }
